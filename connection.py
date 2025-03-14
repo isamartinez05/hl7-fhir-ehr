58ec14e86c00bdf6ef9788501ca104f05ae929cc
isamartinez05 <karoli.martinez@urosario.edu.co>
2025-03-14 15:53:11 -0500

    se modificó el link connection.py

--- connection.py
+++ connection.py
@@ -3,7 +3,7 @@ from pymongo.server_api import ServerApi
 
 
 def connect_to_mongodb(db_name, collection_name):
-    uri = "mongodb+srv://mardugo:$Hola123.@sampleinformationservic.t2yog.mongodb.net/?retryWrites=true&w=majority&appName=SampleInformationService"
+    uri = "mongodb+srv://karolimartinez:1030080400@clusterisa.z8b3g.mongodb.net/?retryWrites=true&w=majority&appName=Clusterisa"
     client = MongoClient(uri, server_api=ServerApi('1'))
     db = client[db_name]
     collection = db[collection_name]
